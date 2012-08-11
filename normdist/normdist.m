c704144cbbdf39dd3218acb6ea933e105df2d9bd
Petko Minkov <petko@foursquare.com>
2012-08-10 21:43:25 -0700

    small updates

--- normdist/normdist.m
+++ normdist/normdist.m
@@ -1,21 +1,25 @@
-counts = zeros(101,1);
+S = 100
+counts = zeros(S + 1,1);
 
 all_sums = [];
 
-N = 20000
+N = 5000 
 
 for n = 1:N
-  a = rand(100,1) > 0.5;
+  a = rand(S,1) > 0.5;
   s = sum(a);
   counts(s + 1) += 1;
   all_sums = [ all_sums s ];
 endfor
+
+plot(counts)
 
 printf("min = %f, max = %f, mean = %f\n", min(all_sums), max(all_sums), mean(all_sums))
 printf("stddev = %f\n", std(all_sums))
 
 m = mean(all_sums);
 stddev = std(all_sums);
+
 for std_mult = 1:3
   in_range = (all_sums > m - stddev * std_mult) & (all_sums < m + stddev * std_mult);
   in_range = in_range .+ 0.0;
