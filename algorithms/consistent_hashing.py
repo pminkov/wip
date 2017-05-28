c2b255dd813b38ea5539233a47d7b54145a46869
Petko Minkov <pminkov@gmail.com>
2017-05-27 20:51:18 -0700

    Fix some problems in consistent hashing

--- algorithms/consistent_hashing.py
+++ algorithms/consistent_hashing.py
@@ -1,25 +1,37 @@
+"""
+Consistent hashing, as described in:
+
+https://www.toptal.com/big-data/consistent-hashing
+"""
 import datetime
 import time
 import hashlib
 import random
 
-DEBUG = 1
+DEBUG = 0
 
 class Server:
   last_server_id = 0
-  width = 10
+  virtual_copies = 50
 
   def __init__(self):
     Server.last_server_id += 1
     self.id = Server.last_server_id
     self.hashes = []
 
-    # Each server has "width" virtual copies.
-    for wi in range(0, Server.width):
+    # Each server has N virtual copies.
+    for wi in range(0, Server.virtual_copies):
       part_id = '{}_{}'.format(self.id, wi)
       self.hashes.append(Cluster.hash(part_id))
 
     self.values = {}
+
+  def dump(self):
+    print '-' * 80
+    print 'ID:', self.id
+    print 'Hashes:', sorted(self.hashes)
+    for (k, v) in self.values.items():
+      print k, v
 
   def store(self, key_hash, value):
     self.values[key_hash] = value
@@ -30,10 +42,16 @@ class Server:
 
     return None
 
-  def move_values(self, cutoff_hash_key, to_server):
+  def _should_move(self, begin_hash, end_hash, key_hash):
+    if begin_hash < end_hash:
+      return begin_hash <= key_hash and key_hash < end_hash
+    else:
+      return begin_hash <= key_hash or key_hash < end_hash
+
+  def move_values(self, to_server, begin_hash, end_hash):
     keys = self.values.keys()
     for k in keys:
-      if k >= cutoff_hash_key:
+      if self._should_move(begin_hash, end_hash, k):
         to_server.store(k, self.values[k])
         del self.values[k]
 
@@ -49,6 +67,40 @@ class Cluster:
     self.hash_to_server_id = {}
     self.server_id_to_server = {}
     pass
+
+  def dump(self):
+    print 'Dumping cluster======='
+    for s in self.servers:
+      s.dump()
+
+  def check(self):
+    is_ok = True
+    for s in self.servers:
+      for key_hash in s.values.keys():
+        # Find if we're in the right server.
+        best_hash = -1
+        max_hash = -1
+        max_server = None
+        which_server = None
+        for ts in self.servers:
+          for hash in ts.hashes:
+            if hash <= key_hash and hash > best_hash:
+              best_hash = hash
+              which_server = ts
+
+            if hash > max_hash:
+              max_hash = hash
+              max_server = ts
+        
+        if best_hash == -1:
+          (best_hash, which_server) = (max_hash, max_server)
+
+        if s != which_server:
+          is_ok = False
+          print 'BAD: {} is in server {} and needs to be in server {}, after server hash {}'. \
+            format(key_hash, s.id, which_server.id, best_hash)
+
+    return is_ok
 
     
   @staticmethod
@@ -97,9 +149,16 @@ class Cluster:
 
     server.store(key_hash, value)
 
+  def in_range(self, begin, end, what):
+    if begin < end:
+      return begin < what and what < end
+    else:
+      return begin < what or what < end
+    
+
   def add_server(self):
     new_server = Server()
-    current_hashes = sorted(self.hash_to_server_id.keys())
+    serving_hashes = sorted(self.hash_to_server_id.keys())
 
     # Set new server in internal structures.
     self.servers.append(new_server)
@@ -110,32 +169,60 @@ class Cluster:
 
     self.server_id_to_server[new_server.id] = new_server
 
-    # Now, see which servers are we going to copy from to the new server.
-    new_server_hashes = sorted(new_server.hashes)
-
-    if len(current_hashes) == 0:
+    if len(serving_hashes) == 0:
       # Empty cluster, first server in.
       return
 
-    move_from = -1
-    for h in new_server_hashes:
-      while move_from + 1 < len(current_hashes) and current_hashes[move_from + 1] <= h:
-        move_from += 1
+    # Now, see which servers are we going to copy from to the new server.
+    new_server_hashes = sorted(new_server.hashes)
 
-      if move_from == -1:
-        real_move_from = len(current_hashes) - 1
-      else:
-        real_move_from = move_from
+    if DEBUG:
+      print '-' * 80
+      print 'ADDING a server'
+      print 'Current hashes: {}'.format(serving_hashes)
+      print 'New server hashes: {}'.format(new_server_hashes)
 
-      if current_hashes[real_move_from] != h:
-        move_from_server_id = self.hash_to_server_id[ current_hashes[real_move_from] ]
-        move_from_server = self.server_id_to_server[ move_from_server_id ]
-        move_from_server.move_values(h, new_server)
+    for i in range(0, len(serving_hashes)):
+      begin_hash = serving_hashes[i]
+      end_hash = serving_hashes[(i + 1) % len(serving_hashes)]
+
+      copy_hashes = []
+      for new_hash in new_server_hashes:
+        if self.in_range(begin_hash, end_hash, new_hash):
+          copy_hashes.append(new_hash)
+
+      if len(copy_hashes) > 0:
         if DEBUG:
-          print 'New server hash: {}. Moving from server hash {}'.format( \
-            h, current_hashes[real_move_from])
-          
-      
+          print 'Moving data from range [{}, {}] to {}'.format(begin_hash, end_hash, copy_hashes)
+        copy_hashes.append(end_hash)
+
+        def sort_value(hash):
+          if begin_hash < end_hash:
+            return hash
+          else:
+            ret = hash
+            if hash <= end_hash:
+              ret += Cluster.mod
+            return ret
+
+        copy_hashes = sorted(copy_hashes, key=sort_value)
+        
+        if DEBUG:
+          print 'Copy points: {}'.format(copy_hashes)
+
+        for ci in range(len(copy_hashes) - 1):
+          begin_copy = copy_hashes[ci]
+          end_copy = copy_hashes[ci + 1]
+
+          origin_server_id = self.hash_to_server_id[begin_hash]
+          origin_server = self.server_id_to_server[origin_server_id]
+
+          if DEBUG:
+            print '   Performing move from server({}) - range [{}, {})'.format( \
+              origin_server.id, begin_copy, end_copy)
+          origin_server.move_values(new_server, begin_copy, end_copy)
+            
+
 
 class RandomWords:
   def __init__(self):
@@ -145,22 +232,54 @@ class RandomWords:
   def random_phrase(self):
     return random.choice(self.words) + "_" + random.choice(self.words)
 
-if __name__ == '__main__':
-  random.seed(None)
+def  simple_test():
+  random.seed(5)
   rw = RandomWords()
+
   cluster = Cluster()
-  
-  cluster.add_server()
-  cluster.add_server()
   cluster.add_server()
 
-  cluster.set(random.randint(1,1000), rw.random_phrase())
+  for i in range(0, 10):
+    cluster.set(random.randint(1, 1 << 30), rw.random_phrase())
+
+  cluster.add_server()
+  cluster.check()
+
+  cluster.dump()
+  cluster.add_server()
+
+  cluster.dump()
+
+  for s in cluster.servers:
+    print s.id, len(s.values)
+
+  assert cluster.check()
+
+def big_test():
+  random.seed(5)
+  rw = RandomWords()
+
+  cluster = Cluster()
+  cluster.add_server()
+
+  N = 10
+  for i in range(N):
+    cluster.set(random.randint(1, 1 << 30), rw.random_phrase())
+
+  for i in range(8):
+    cluster.add_server()
+    print i
+    assert cluster.check()
+
+  for s in cluster.servers:
+    print s.id, len(s.values)
 
 
-  cluster.set(5, "five")
 
-  print cluster.get(5)
+if __name__ == '__main__':
+  #simple_test()
+  big_test()
 
-  cluster.set(1000, "hiliada")
 
-  print cluster.get(1000)
+
+
